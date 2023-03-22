4155c5dfc0ef88869217da3fc1487088a1eefd07
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2023-03-22 18:12:09 +0900

    03.22

--- src/j03_Exception/Ex08_MyExceptionChecked.java
+++ src/j03_Exception/Ex08_MyExceptionChecked.java
@@ -18,12 +18,12 @@ class AgeException extends Exception {
 public class Ex08_MyExceptionChecked {
 	// ** 나이 입력받아 return 하는 메서드 만들기
 	// => 예외처리코드를 컴파일시에 반드시 확인함
-	public static int readAge() throws AgeException, InputMismatchException {
+	public static int readAge() throws AgeException, InputMismatchException {    // 예외 처리를 AgeException,InputMismatchException 떠넘김 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("** 나이를 입력 하세요 => ");
 		int age = sc.nextInt(); //문자입력시 -> InputMismatchException 
 		sc.close();
-		if (age<1 || age>200) throw new AgeException();
+		if (age<1 || age>200) throw new AgeException();	// 새로운 예외 처리를 만듬 
 		else return age;
 	} //readAge
 
